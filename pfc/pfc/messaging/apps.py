2a8bae552493c757980eac97dafb0fd0f2e6bb77
Daniel Salazar <daniel.salazar@qustodio.com>
2017-12-03 16:19:05 +0100

    Adds migration for messaging model

--- pfc/pfc/messaging/apps.py
+++ pfc/pfc/messaging/apps.py
@@ -4,4 +4,4 @@ from django.apps import AppConfig
 
 
 class MessagingConfig(AppConfig):
-    name = 'messaging'
+    name = 'pfc.messaging'
--- pfc/pfc/messaging/models.py
+++ pfc/pfc/messaging/models.py
@@ -1,5 +1,18 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from pfc.users.models import User
+
+
+class Message(models.Model):
+    id = models.AutoField(primary_key=True)
+    subject = models.CharField(_('Message\'s subject'), blank=False, max_length=255)
+    body = models.TextField(_("Message's body"), blank=False)
+
+    author_id = models.ForeignKey(User, related_name='sent_messages')
+    destination_id = models.ForeignKey(User, related_name='received_messages')
+
+    def __str__(self):
+        return self.subject
